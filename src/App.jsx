04c4d1de27f1033a02817c6d864b29d30f83e111
Sefa Sayıcı <sefa78204@gmail.com>
2024-02-01 15:27:20 +0300

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,67 @@ import Confetti from 'react-confetti';
 import Fuse from 'fuse.js';
 
 
-// Uygulama ana bileşeni içindeki tüm elemanları sarmak için bir div kullanıyoruz
 
 const AppWrapper = styled.div`
-  max-width: 600px;
+  width: 1000px;
   margin: auto;
   padding: 20px;
+  color: #000;
 `;
 
 const StyledButton = styled(Button)`
   margin-top: 10px;
+  display: block;
+  margin: 0 auto;
 `;
 
 const StyledTable = styled(Table)`
   margin-top: 20px;
 `;
 
+const SectionWithBackground = styled.section`
+  background-image: url("../src/assets/photo-1614521084980-811d04f6c6cb.avif");
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+  /* Diğer background özelliklerini isteğe bağlı olarak ekleyebilirsiniz. */
+`;
+
+const InputGroupWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+const FilterFormWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap; /* Eğer sığmazsa alt satıra geçmesini sağlamak için wrap özelliğini ekleyin */
+  gap: 10px; /* Elemanlar arasındaki boşluğu ayarlamak için gap ekleyin */
+  align-items: center;
+  
+`;
+
+const FilterFormGroup = styled(Form.Group)`
+  flex: 1; /* Eşit genişlikte paylaştırmak için flex özelliğini ekleyin */
+   /* Sıralamayı ayarlamak için order ekleyin */
+`;
+
+const FilterStatusWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+`;
+
+
+
+
 // Ana uygulama bileşeni olan 'App' bileşeni.
 function App() {
-  // Mağazaları temsil eden bir dizi. Her mağaza bir 'id' ve 'name' özelliğine sahiptir.
+  // Mağazaları ve kategorileri temsil eden diziler.
   const shops = [
     { "id": 1, "name": "Teknosa" },
     { "id": 2, "name": "Eczane" },
@@ -35,7 +77,6 @@ function App() {
     { "id": 6, "name": "A-101" },
   ];
 
-  // Kategorileri temsil eden bir dizi. Her kategori bir 'id' ve 'name' özelliğine sahiptir.
   const categories = [
     { "id": 1, "name": "Elektronik" },
     { "id": 2, "name": "Sağlık" },
@@ -45,10 +86,10 @@ function App() {
     { "id": 6, "name": "Kasap" },
   ];
 
-  // Yeni bir ürün eklemek için kullanılan fonksiyonlar.
+  // Ürün ekleme için kullanılan state'ler.
   const [productName, setProductName] = useState('');
-  const [selectedShop, setSelectedShop] = useState(1);
-  const [selectedCategory, setSelectedCategory] = useState(1);
+  const [selectedShop, setSelectedShop] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [products, setProducts] = useState([]);
   const [confettiVisible, setConfettiVisible] = useState(false);
 
@@ -110,7 +151,7 @@ function App() {
   }, [products]);
 
   // Filtreleme için kullanılan state'ler
-  const [filteredShopId, setFilteredShopId] = useState('all');
+  const [filteredShopId, setFilteredShopId] = useState('all');  
   const [filteredCategoryId, setFilteredCategoryId] = useState('all');
   const [filteredStatus, setFilteredStatus] = useState('all');
   const [filteredName, setFilteredName] = useState('');
@@ -160,154 +201,173 @@ function App() {
   }, [filteredShopId, filteredCategoryId, filteredStatus, filteredName, products]);
 
   // JSX olarak render edilen bileşenler.
+
+
   return (
     <>
-      {/* Konfeti animasyonunu temsil eden bileşen. */}
-      <Container>
-        
-     
-      <Confetti
-        width={window.innerWidth}
-        height={window.innerHeight}
-        numberOfPieces={confettiVisible ? 300 : 0}
-        recycle={true}
-      />
-      {/* Uygulama bileşeni içindeki tüm diğer bileşenleri saran bir 'div'. */}
-      <AppWrapper>
-      
-        {/* Ürün eklemek için giriş formu. */}
-        <InputGroup className="mb-3 py-3">
-          <InputGroup.Text id="basic-addon1">Ürün Adı</InputGroup.Text>
-          <Form.Control
-            placeholder="Ürün Giriniz..."
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-          />
-        </InputGroup>
+      <SectionWithBackground>
 
-        {/* Mağaza seçimi için dropdown. */}
-        <Form.Select
-          aria-label="Mağaza Seç"
-          className='mb-2'
-          value={selectedShop}
-          onChange={(e) => setSelectedShop(parseInt(e.target.value))}
-        >
-          <option>MARKET</option>
-          {shops.map((shop) => (
-            <option key={shop.id} value={shop.id}>{shop.name}</option>
-          ))}
-        </Form.Select>
+        {/* Konfeti animasyonunu temsil eden bileşen. */}
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          numberOfPieces={confettiVisible ? 300 : 0}
+          recycle={true}
+        />
 
-        {/* Kategori seçimi için dropdown. */}
-        <Form.Select
-          aria-label="Kategori Seç"
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(parseInt(e.target.value))}
-        >
-          <option>KATEGORİ</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </Form.Select>
+        {/* Ana uygulama bileşenini saran bir 'div'. */}
+        <AppWrapper>
 
-        {/* Ürün eklemek için buton. */}
-        <StyledButton  variant="primary mb-2 mt-2" onClick={handleAddProduct}>Ürünü Ekle</StyledButton>
+          {/* Ürün adını girmek için giriş formu. */}
+          <InputGroupWrapper>
+            <InputGroup className="mb-3 py-3">
+              <InputGroup.Text id="basic-addon1">Ürün Adı</InputGroup.Text>
+              <Form.Control
+                placeholder="Ürün Giriniz..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+              />
+            </InputGroup>
 
-        <div className="mb-3">
-          <Form.Group controlId="filterShop">
-            <Form.Label>Market</Form.Label>
+            {/* Mağaza seçimi için açılır menü. */}
             <Form.Select
-              value={filteredShopId}
-              onChange={(e) => setFilteredShopId(e.target.value)}
+              aria-label="Mağaza Seç"
+              className='mb-3'
+              value={selectedShop}
+              onChange={(e) => setSelectedShop(parseInt(e.target.value))}
             >
-              {shopOptions.map((shop) => (
+              <option>MARKET</option>
+              {/* Mağazaların listesi üzerinde dönerek seçenekleri oluştur. */}
+              {shops.map((shop) => (
                 <option key={shop.id} value={shop.id}>{shop.name}</option>
               ))}
             </Form.Select>
-          </Form.Group>
 
-          <Form.Group controlId="filterCategory">
-            <Form.Label>Kategori</Form.Label>
+            {/* Kategori seçimi için açılır menü. */}
             <Form.Select
-              value={filteredCategoryId}
-              onChange={(e) => setFilteredCategoryId(e.target.value)}
+              aria-label="Kategori Seç"
+              className='mb-3'
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(parseInt(e.target.value))}
             >
-              {categoryOptions.map((category) => (
-                <option key={category.id} value={category.id}>{category.name}</option>
+              <option>KATEGORİ</option>
+              {/* Kategorilerin listesi üzerinde dönerek seçenekleri oluştur. */}
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
               ))}
             </Form.Select>
-          </Form.Group>
+          </InputGroupWrapper>
+          {/* Ürün eklemek için buton. */}
+          
+          <StyledButton variant="primary mb-2 mt-2" onClick={handleAddProduct}>Ürünü Ekle</StyledButton>
+          
+          <div className="mb-3">
+            {/* Filtreleme formu */}
+            <FilterFormWrapper>
+              <FilterFormGroup controlId="filterName">
+                <Form.Label className=' text-white  fw-bold'>Ürün Adı</Form.Label>
+                {/* Ürün adı filtresi için metin girişi. */}
+                <Form.Control
+                  type="text"
+                  value={filteredName}
+                  onChange={(e) => setFilteredName(e.target.value)}
+                />
+              </FilterFormGroup>
 
-          <Form.Group controlId="filterStatus">
-            <Form.Label>Durum</Form.Label>
-            {statusOptions.map((status) => (
-              <Form.Check
-                key={status.id}
-                type="radio"
-                label={status.name}
-                value={status.id}
-                checked={filteredStatus === status.id}
-                onChange={(e) => setFilteredStatus(e.target.value)}
-              />
-            ))}
-          </Form.Group>
+              <FilterFormGroup controlId="filterShop">
+                <Form.Label className='text-white fw-bold'>Market</Form.Label>
+                {/* Market filtresi için açılır menü. */}
+                <Form.Select
+                  value={filteredShopId}
+                  onChange={(e) => setFilteredShopId(e.target.value)}
+                >
+                  {/* Tüm mağazaları gösteren seçenek ve diğer mağazaları listele. */}
+                  {shopOptions.map((shop) => (
+                    <option key={shop.id} value={shop.id}>{shop.name}</option>
+                  ))}
+                </Form.Select>
+              </FilterFormGroup>
 
-          <Form.Group controlId="filterName">
-            <Form.Label>Ürün Adı</Form.Label>
-            <Form.Control
-              type="text"
-              value={filteredName}
-              onChange={(e) => setFilteredName(e.target.value)}
-            />
-          </Form.Group>
+              <FilterFormGroup controlId="filterCategory">
+                <Form.Label className='text-white fw-bold'>Kategori</Form.Label>
+                {/* Kategori filtresi için açılır menü. */}
+                <Form.Select
+                  value={filteredCategoryId}
+                  onChange={(e) => setFilteredCategoryId(e.target.value)}
+                >
+                  {/* Tüm kategorileri gösteren seçenek ve diğer kategorileri listele. */}
+                  {categoryOptions.map((category) => (
+                    <option key={category.id} value={category.id}>{category.name}</option>
+                  ))}
+                </Form.Select>
+              </FilterFormGroup>
 
-          {/* <StyledButton  variant="primary" onClick={handleFilterApply}>
-            Filtrele
-          </StyledButton> */}
-        </div>
+              <FilterStatusWrapper>
+                <FilterFormGroup controlId="filterStatus">
+                  <Form.Label className='text-white fw-bold'>Durum</Form.Label>
+                  {/* Durum filtresi için radyo düğmeleri. */}
+                  {statusOptions.map((status) => (
+                    <Form.Check className='d-flex gap-1 text-white fw-bold'
+                      key={status.id}
+                      type="radio"
+                      label={status.name}
+                      value={status.id}
+                      checked={filteredStatus === status.id}
+                      onChange={(e) => setFilteredStatus(e.target.value)}
+                    />
+                  ))}
+                </FilterFormGroup>
+              </FilterStatusWrapper>
 
-        {/* Filtrelenmiş ürünlerin listesi */}
-        <StyledTable  striped bordered hover>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Ürün Adı</th>
-              <th>Mağaza</th>
-              <th>Kategori</th>
-              <th>Satın Alındı</th>
-              <th>İşlemler</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredProducts.map((product) => (
-              <tr key={product.id} style={{ textDecoration: product.isBought ? 'line-through' : 'none' }}>
-                <td>{product.id}</td>
-                <td>{product.name}</td>
-                <td>{product.shop.name}</td>
-                <td>{product.category.name}</td>
-                <td>
-                  <Form.Check
-                    type="checkbox"
-                    label=""
-                    checked={product.isBought}
-                    onChange={() => handleToggleBought(product.id)}
-                  />
-                </td>
-                <td>
-                  <StyledButton  variant="danger" onClick={() => handleDeleteProduct(product.id)}>
-                    Sil
-                  </StyledButton>
-                </td>
+            </FilterFormWrapper>
+          </div>
+
+          {/* Ürünleri listeleyen tablo. */}
+          <StyledTable striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Ürün Adı</th>
+                <th>Mağaza</th>
+                <th>Kategori</th>
+                <th>Satın Alındı</th>
+                <th>İşlemler</th>
               </tr>
-            ))}
-          </tbody>
-        </StyledTable>
-      </AppWrapper>
-      </Container>
+            </thead>
+            <tbody>
+              {/* Filtrelenmiş ürünleri listeleyen satırlar. */}
+              {filteredProducts.map((product) => (
+                <tr key={product.id} style={{ textDecoration: product.isBought ? 'line-through' : 'none' }}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.shop.name}</td>
+                  <td>{product.category.name}</td>
+                  <td>
+                    {/* Ürünün satın alındı durumunu değiştirmek için checkbox. */}
+                    <Form.Check
+                      type="checkbox"
+                      label=""
+                      checked={product.isBought}
+                      onChange={() => handleToggleBought(product.id)}
+                    />
+                  </td>
+                  <td>
+                    {/* Ürünü silmek için düğme. */}
+                    <StyledButton variant="danger" onClick={() => handleDeleteProduct(product.id)}>
+                      Sil
+                    </StyledButton>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </StyledTable>
+        </AppWrapper>
+
+      </SectionWithBackground>
     </>
   )
 }
